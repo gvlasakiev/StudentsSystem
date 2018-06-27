bac7234d1cb69723fc1c77cefb90d436a33c5610
samuilz <samuilzahariev96@gmail.com>
2018-06-27 16:45:23 +0300

    Created StudentClass and Parent classes.

---
+++ student-system/src/models/Parent.java
@@ -0,0 +1,12 @@
+package models;
+
+public class Parent extends Person {
+    //List<Student> children;
+
+    @Override
+    protected void showInfo() {
+        System.out.printf("Full name: %s %s\n", super.getFirstName(), super.getLastName());
+        System.out.printf("Phone number: %s\n", super.getPhoneNumber());
+        System.out.printf("Email: %s", super.getEmail());
+    }
+}
