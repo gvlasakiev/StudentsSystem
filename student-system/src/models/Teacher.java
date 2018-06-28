f8e73b991a239a8ea1030a2d003f1b4d0e6e14dc
Hristiyan Todorov <hristiyant@gmail.com>
2018-06-28 12:43:20 +0300

    Added hasStudentClass and hasStudent to Teacher class.

--- student-system/src/models/Teacher.java
+++ student-system/src/models/Teacher.java
@@ -3,36 +3,34 @@ package models;
 import java.util.ArrayList;
 import java.util.Map;
 
-
 public class Teacher extends Person {
-	
+
 	private ArrayList<StudentClass> listOfClasses;
 	private ArrayList<Subject> listOfSubjects;
-	
+
 	private Map<StudentClass, Subject> studentClassSubjects;
-	
-	public Teacher(){
+
+	public Teacher() {
 		super();
 		listOfClasses = new ArrayList<>();
 		listOfSubjects = new ArrayList<>();
 	}
-	
+
 	@Override
 	protected void showInfo() {
-			System.out.printf("Full name: %s %s\n", super.getFirstName(), super.getLastName());
-	        System.out.printf("Phone number: %s\n", super.getPhoneNumber());
-	        System.out.printf("Email: %s\n", super.getEmail());
-	        System.out.println("List of all classes: ");
-	        
-	        //Can use StreamingAPI -- >
-	        for (StudentClass studentClass : listOfClasses) {
-				System.out.println(studentClass);
-			}
-	        System.out.println("\nList of all subjects: ");
-	        for (Subject subject : listOfSubjects) {
-				System.out.println(subject);
-			}
-		
+		System.out.printf("Full name: %s %s\n", super.getFirstName(), super.getLastName());
+		System.out.printf("Phone number: %s\n", super.getPhoneNumber());
+		System.out.printf("Email: %s\n", super.getEmail());
+		System.out.println("List of all classes: ");
+
+		// Can use StreamingAPI -- >
+		for (StudentClass studentClass : listOfClasses) {
+			System.out.println(studentClass);
+		}
+		System.out.println("\nList of all subjects: ");
+		for (Subject subject : listOfSubjects) {
+			System.out.println(subject);
+		}
 
 	}
 
@@ -43,12 +41,32 @@ public class Teacher extends Person {
 	public ArrayList<Subject> getListOfSubjects() {
 		return listOfSubjects;
 	}
-	
-	public void addGrade(Student student, Subject subject, Grade grade){
-		//Check here if teacher has this student in the listOfClasses and subject in listOfSubjects
-		
-		if(student.getSubjectGrade().containsKey(subject)) {
-			student.getSubjectGrade().get(subject).add(grade);
+
+	public void addGrade(Student student, Subject subject, Grade grade) {
+		// Check if teacher has this student in the listOfClasses and
+		// subject in listOfSubjects then add grade
+		if (this.hasStudentClass(student.getStudentClass()) && this.hasStudent(student)) {
+			if (student.getSubjectGrade().containsKey(subject)) {
+				student.getSubjectGrade().get(subject).add(grade);
+			}
 		}
+	}
+
+	// Check if Teacher has studentClass
+	public boolean hasStudentClass(StudentClass studentClass) {
+		if (this.getListOfClasses().contains(studentClass)) {
+			return true;
+		}
+		return false;
+	}
+
+	// Check if Teacher has student
+	public boolean hasStudent(Student student) {
+		if (this.hasStudentClass(student.getStudentClass())) {
+			if (student.getStudentClass().getStudents().contains(student)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
