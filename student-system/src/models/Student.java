c6cfc789e9d7ab2b2dacc3e08b8290d13ee6da57
Georvi Vlasakiev <g.vlasakiev@gmail.com>
2018-06-28 11:21:27 +0300

    Added Student class

---
+++ student-system/src/models/Student.java
@@ -0,0 +1,80 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Student extends Person {
+    private String eng;
+    private int classNumber;
+    private Parent parent;
+    private List<String> remarks;
+    private List<Subject> subjects;
+    private Map<Subject, List<Grade>> subjectGrade;
+
+    public Student(String egn, int classNumber, Parent parent, List<String> remarks, List<Subject> subjects, Map<Subject, List<Grade>> subjectGrade) {
+        setEng(egn);
+        setClassNumber(classNumber);
+        setParent(parent);
+        remarks = new ArrayList<String>();
+        subjects = new ArrayList<Subject>();
+        subjectGrade = new HashMap<Subject, List<Grade>>();
+    }
+
+
+    @Override
+    protected void showInfo() {
+        System.out.printf("Full name: %s %s\n", super.getFirstName(), super.getLastName());
+        System.out.printf("Phone number: %s\n", super.getPhoneNumber());
+        System.out.printf("Email: %s", super.getEmail());
+    }
+
+    public String getEng() {
+        return eng;
+    }
+
+    private void setEng(String eng) {
+        this.eng = eng;
+    }
+
+    public int getClassNumber() {
+        return classNumber;
+    }
+
+    private void setClassNumber(int classNumber) {
+        this.classNumber = classNumber;
+    }
+
+    public Parent getParent() {
+        return parent;
+    }
+
+    private void setParent(Parent parent) {
+        this.parent = parent;
+    }
+
+    public List<String> getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(List<String> remarks) {
+        this.remarks = remarks;
+    }
+
+    public List<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(List<Subject> subjects) {
+        this.subjects = subjects;
+    }
+
+    public Map<Subject, List<Grade>> getSubjectGrade() {
+        return subjectGrade;
+    }
+
+    public void setSubjectGrade(Map<Subject, List<Grade>> subjectGrade) {
+        this.subjectGrade = subjectGrade;
+    }
+}
