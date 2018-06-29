3bb3ba648221d93997c015599e907b8a19cd9d81
samuilz <samuilzahariev96@gmail.com>
2018-06-29 22:38:32 +0300

    Added some fields at StudentClass class.

--- student-system/src/models/StudentClass.java
+++ student-system/src/models/StudentClass.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 class StudentClass {
+    private ClassBook ClassBook;
     private List<Student> students;
+    private List<Subject> subjects;
     private String name;
     // private dnevnik dnevnik
 
